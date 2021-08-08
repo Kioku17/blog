3628dd73f918e29cc123f7cc152d7643a6575499
Kioku17 <63692852+Kioku17@users.noreply.github.com>
2021-08-08 10:29:50 +0700

    Update loadpage.js

--- assets/js/loadpage.js
+++ assets/js/loadpage.js
@@ -1,44 +1,31 @@
-$('body').on("click", 'a', function(loadpage) {
-$(".effect").show("slow");
-loadpage.preventDefault();
-var url = $(this).attr('href');
-BlogPage(url, false);
-});
-
-$(".effect").hide("slow");
-
-var state = {
-name: location.href,
-page: document.title
-};
-history.pushState(state, document.title, location.href);
-$(window).on("popstate", function(){
-if(history.state){
-BlogPage(history.state.name, true);}});
-var BlogPage = function(link,pop = false){
-$.fn.url = link;
-var request = {
-type: 'GET',
-dataType: 'html',
-url: link
-};
-$.ajax(request).done(function(data){
-var title = data.split('<title>')[1].split('</title>')[0];
-var body = data.split('<BlogPage>')[1].split('</BlogPage>')[0];
-$(".effect").hide("slow");
-if(pop != true){
-var state = {
-name: link,
-page: ''
-};
-history.pushState(state, null, state.name);
-}
-$("title").text(title);
-$("BlogPage").html(body);
-$('html,body').animate({scrollTop:0},200);
-}).fail(function(){
-$("BlogPage").html(body);
-})
-}
-
-$(".effect").hide("slow");
+$('body').on("click", 'a', function(loadpage) { 
+$(".effect").show("slow"); 
+$(".left_load").show("slow"); 
+$(".left").hide("slow"); loadpage.preventDefault(); 
+url = $(this).attr('href'); BlogPage(url, false); });
+$(".effect").hide("slow"); 
+$(".left_load").hide("slow"); 
+var state = {name: location.href, page: document.title}; 
+window.history.pushState(state, document.title, location.href); 
+$(window).on("popstate", function(){ if(history.state){ 
+BlogPage(history.state.name, true); 
+} }); 
+function BlogPage(link,pop){ $("BlogPage").append('
+'); 
+$.get(link,"", function(data_html){ 
+var title = data_html.split('')[0]; 
+var body = data_html.split('')[1].split('')[0]; 
+$("title").text(title); 
+$("BlogPage").html(body); 
+$(".effect").hide("slow"); 
+$(".left_load").hide("slow"); 
+$(".right").show("slow"); 
+$(".left").show("slow"); 
+if(pop != true){ 
+var state = {name: link, page: title}; 
+window.history.pushState(state, title, link+'#me'); 
+} 
+$('html,body').animate({scrollTop:0},200); }); } 
+$(".left").show("slow"); 
+$(".effect").hide("slow"); 
+$(".left_load").hide("slow");
